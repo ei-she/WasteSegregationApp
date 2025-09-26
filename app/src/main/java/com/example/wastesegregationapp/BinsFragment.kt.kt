3133c69733ec113a08e3cb53c0e0f41dd9a9a113
tan <loyaga.christian.gumaod@gmail.com>
2025-09-26 15:16:43 +0800

    dadsads

---
+++ app/src/main/java/com/example/wastesegregationapp/BinsFragment.kt.kt
@@ -0,0 +1,15 @@
+package com.example.wastesegregationapp
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class BinsFragment : Fragment() {
+        override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
+        ): View? {
+            return inflater.inflate(R.layout.fragment_bins, container, false)
+        }
+    }
